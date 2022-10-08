bbcf107917e1b1bee4225e6803992d2610e44034
wangkanghong123 <314404923@qq.com>
2022-10-08 18:47:20 +0800

    commit config and source

---
+++ Source/localizationTestC/localizationTestCGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "localizationTestCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOCALIZATIONTESTC_API AlocalizationTestCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
